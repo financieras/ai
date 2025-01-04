fce14ed45b56a72e92e31674d7bd11e88df06744
financieras <financieras@gmail.com>
2025-01-04 14:37:51 +0100

    initial analysis

---
+++ logistic_regression/src/initial_analysis.py
@@ -0,0 +1,33 @@
+import os
+import aux.colors as c
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def initial_exploration(input_file='../datasets/dataset_train.csv'):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(input_file, index_col=0)
+    # index_col=0 indica que la primera columna del archivo CSV contiene el índice del DataFrame
+
+    print(f"\n{c.BLUE}Info about the file: {input_file}{c.RESET}\n")
+    df.info()
+
+    input(f"\n{c.YELLOW}Press ENTER to continue...{c.RESET}")
+
+    print(df.head(10))  # show the first rows
+
+    print(f"\n{c.BLUE}Null values by column{c.RESET}\n")
+    print(df.isnull().sum())
+
+    # Analysis of unique values for 'Hogwarts House'
+    print("\nUnique values in Hogwarts House:")
+    print(df['Hogwarts House'].value_counts())
+
+    # Analysis of unique values for 'Best Hand'
+    print("\nUnique values in Best Hand:")
+    print(df['Best Hand'].value_counts())
+
+
+if __name__ == "__main__":
+    initial_exploration()
--- logistic_regression/src/main.py
+++ logistic_regression/src/main.py
@@ -1,5 +1,6 @@
 import os
 import aux.colors as c
+import initial_analysis
 import preprocessing
 import correlations
 import describe
@@ -17,15 +18,16 @@ def pause():
 def show_menu():
     text_menu = f"""
     {c.PURPLE}===========   M E N U   ===========
-    {c.CYAN}1.{c.BLUE} Preprocesing data
-    {c.CYAN}2.{c.BLUE} Detect highly correlated columns
-    {c.CYAN}3.{c.BLUE} Describe dataset (max, min, ...)
-    {c.CYAN}4.{c.BLUE} Histogram
-    {c.CYAN}5.{c.BLUE} Scatter Plot
-    {c.CYAN}6.{c.BLUE} Pair Plot
-    {c.CYAN}7.{c.BLUE} Normalize dataset
-    {c.CYAN}8.{c.BLUE} Option 8
-    {c.CYAN}9.{c.BLUE} Option 9
+    {c.CYAN}1.{c.BLUE} Initial data analysis
+    {c.CYAN}2.{c.BLUE} Preprocesing data
+    {c.CYAN}3.{c.BLUE} Detect highly correlated columns
+    {c.CYAN}4.{c.BLUE} Describe dataset (max, min, ...)
+    {c.CYAN}5.{c.BLUE} Histogram
+    {c.CYAN}6.{c.BLUE} Scatter Plot
+    {c.CYAN}7.{c.BLUE} Pair Plot
+    {c.CYAN}8.{c.BLUE} Normalize dataset
+    {c.CYAN}9.{c.BLUE} Option 8
+    {c.CYAN}10.{c.BLUE} Option 9
     {c.CYAN}0. Exit
     {c.PURPLE}{'='*36}{c.RESET}
     """
@@ -37,24 +39,27 @@ def execute_option(choice):
         print(f"{c.PURPLE}Bye! 👋{c.RESET}\n")
         return False
     elif choice == 1:
+        print(f"{c.CYAN}=== Initial data analysis ==={c.RESET}\n")
+        initial_analysis.initial_exploration()
+    elif choice == 2:
         print(f"{c.CYAN}=== Preprocessing Data ==={c.RESET}\n")
         preprocessing.preprocess_data()
-    elif choice == 2:
+    elif choice == 3:
         print(f"{c.CYAN}=== Detecting Highly Correlated Columns ==={c.RESET}\n")
         correlations.detect_highly_correlated_columns()
-    elif choice == 3:
+    elif choice == 4:
         print(f"{c.CYAN}=== Analyzing Dataset ==={c.RESET}\n")
         describe.analyze_dataset()
-    elif choice == 4:
+    elif choice == 5:
         print(f"{c.CYAN}=== Making the histogram (wait a moment...) ==={c.RESET}\n")
         histogram.generate_histogram()
-    elif choice == 5:
+    elif choice == 6:
         print(f"{c.CYAN}=== Making the Scatter Plot (wait a moment...) ==={c.RESET}\n")
         scatter_plot.generate_scatter_plot()
-    elif choice == 6:
+    elif choice == 7:
         print(f"{c.CYAN}=== Making the Pair Plot (wait a moment...) ==={c.RESET}\n")
         pair_plot.exploring_feature_relationships()
-    elif choice == 7:
+    elif choice == 8:
         print(f"{c.CYAN}=== Normalizing Dataset ==={c.RESET}\n")
         X_train, X_val, X_test, y_train, y_val, y_test = normalize.prepare_data()
     else:
